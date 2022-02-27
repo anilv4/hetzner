8ca73f7c3273a9a31110bacd13373938d05d0801
Anil Vettathu <anilv.in@gmail.com>
2022-02-27 16:55:41 +0530

    corrected ocp-tools-install

--- ocp-tools-setup/ocp-tools-install.sh
+++ ocp-tools-setup/ocp-tools-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 OCP_VERSION="4.7.44"
 
-TMPDIR=$(mkdir -d)
+read -p "***OCP lient tool version is $OCP_VERSION. CTRL+C to exit***"
+
+TMPDIR=$(mktemp -d)
 URL_OCP_CLIENT="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/"$OCP_VERSION"/openshift-client-linux.tar.gz"
 
 curl $URL_OCP_CLIENT -o "$TMPDIR/openshift-client-linux.tar.gz"
