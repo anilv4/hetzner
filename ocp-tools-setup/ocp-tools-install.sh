bb2fa0385950917b4108fb976ecda206b476815b
Anil Vettathu <anilv.in@gmail.com>
2022-03-01 02:27:45 +0530

    date

--- ocp-tools-setup/ocp-tools-install.sh
+++ ocp-tools-setup/ocp-tools-install.sh
@@ -4,7 +4,14 @@ OCP_VERSION="4.7.44"
 
 read -p "***OCP client tool version is $OCP_VERSION. CTRL+C to exit***"
 
-mkdir -pv ~/.local/bin/
+#RHEL8
+BINDIR="$HOME/bin"
+
+#Fedora
+#BINDIR="$HOME/.local/bin"
+
+mkdir -pv $BINDIR
+
 TMPDIR=$(mktemp -d)
 
 URL_OCP_CLIENT="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/"$OCP_VERSION"/openshift-client-linux.tar.gz"
@@ -18,7 +25,7 @@ tar xavf "$TMPDIR/openshift-install-linux.tar.gz" --directory "$TMPDIR"
 
 ls $TMPDIR/{oc,kubectl,openshift-install}
 
-mv -v $TMPDIR/{oc,kubectl,openshift-install} ~/.local/bin/
+mv -v $TMPDIR/{oc,kubectl,openshift-install} $BINDIR
 
 rm -rfv $TMPDIR
 
